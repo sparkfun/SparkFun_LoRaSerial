3048b9a5e8db99be35f7e7d3d8a12d44e7a6c894
Nathan Seidle <nathan@sparkfun.com>
2022-03-26 14:47:09 -0600

    Add boardID ADC

--- Firmware/LoRaSerial_Firmware/Begin.ino
+++ Firmware/LoRaSerial_Firmware/Begin.ino
@@ -1,47 +1,16 @@
 //Based on hardware features, determine which hardware this is
 void beginBoard()
 {
-  //Use ADC to check resistor divider
-#if defined(ARDUINO_AVR_UNO)
-  pin_cs = 7;
-  pin_dio0 = 3;
-  pin_dio1 = 2;
-  pin_txen = 5;
-  pin_rxen = 8;
-  pin_rst = A2;
-  pin_cts = 9;
-  pin_rts = 10;
-  pin_activityLED = 6;
-  pin_linkLED = 4;
-
-  //Flow control
-  pinMode(pin_rts, OUTPUT);
-  digitalWrite(pin_rts, HIGH);
-
-  pinMode(pin_cts, INPUT_PULLUP);
-
-  pinMode(pin_activityLED, OUTPUT);
-  digitalWrite(pin_activityLED, LOW);
-
-  pinMode(pin_linkLED, OUTPUT);
-  digitalWrite(pin_linkLED, LOW);
-
-  strcpy(platformPrefix, "ATMEGA328 1W");
-
-#elif defined(ARDUINO_ARCH_ESP32)
+#if defined(ARDUINO_ARCH_ESP32)
   //Lower power boards
   pin_cs = 15;
   pin_dio0 = 26; //aka A0
   pin_dio1 = 25; //aka A1
-  pin_txen = 255; //Not used
-  pin_rxen = 255;
   pin_rst = 32;
-  pin_cts = 255;
-  pin_rts = 255;
 
   pin_trigger = 13;
 
-  pin_setupButton = 0;
+  pin_trainButton = 0;
 
   //Debug
   pinMode(pin_trigger, OUTPUT);
@@ -50,20 +19,28 @@ void beginBoard()
   strcpy(platformPrefix, "ESP32 100mW");
 
 #elif defined(ARDUINO_ARCH_SAMD)
+
+  //Use ADC to check resistor divider
+  pin_boardID = A2;
+
   pin_cs = 5;
   pin_dio0 = 7; //aka A0
   pin_dio1 = 10; //aka A1
   pin_txen = 2;
   pin_rxen = 3;
   pin_rst = 6;
-  pin_cts = 255; //TODO
-  pin_rts = 255;
-  pin_activityLED = 9;
-  pin_linkLED = 8;
-  pin_txLED = 255; //TODO
-  pin_rxLED = 255;
+  pin_cts = 30;
+  pin_rts = 38;
+  pin_txLED = 31;
+  pin_rxLED = A5;
+  pin_rssi1LED = A3;
+  pin_rssi2LED = A4;
+  pin_rssi3LED = 8;
+  pin_rssi4LED = 9;
 
-  pin_trigger = A2;
+  pin_trainButton = 4;
+
+  pin_trigger = A0;
 
   //Flow control
   pinMode(pin_rts, OUTPUT);
@@ -72,33 +49,66 @@ void beginBoard()
   pinMode(pin_cts, INPUT_PULLUP);
 
   //LEDs
-  pinMode(pin_activityLED, OUTPUT);
-  digitalWrite(pin_activityLED, LOW);
+  pinMode(pin_rssi1LED, OUTPUT);
+  digitalWrite(pin_rssi1LED, LOW);
+  pinMode(pin_rssi2LED, OUTPUT);
+  digitalWrite(pin_rssi2LED, LOW);
+  pinMode(pin_rssi3LED, OUTPUT);
+  digitalWrite(pin_rssi3LED, LOW);
+  pinMode(pin_rssi4LED, OUTPUT);
+  digitalWrite(pin_rssi4LED, LOW);
 
-  pinMode(pin_linkLED, OUTPUT);
-  digitalWrite(pin_linkLED, LOW);
+  pinMode(pin_txLED, OUTPUT);
+  digitalWrite(pin_txLED, LOW);
+  pinMode(pin_rxLED, OUTPUT);
+  digitalWrite(pin_rxLED, LOW);
+
+  //Train button input
+  pinMode(pin_trainButton, INPUT_PULLUP);
 
   //Debug
   pinMode(pin_trigger, OUTPUT);
   digitalWrite(pin_trigger, HIGH);
 
-  strcpy(platformPrefix, "SAMD21 1W");
+  //Get average of board ID voltage divider
+  int val = 0;
+  for (int x = 0 ; x < 8 ; x++)
+    val += analogRead(pin_boardID);
+  val /= 8;
+
+  //Convert ADC to volts
+  float boardID = 3.3 * val / 1024;
+
+  //Use ADC to check board ID resistor divider
+  if (boardID > 1.64 * 0.9 && boardID < 1.64 * 1.1)
+  {
+    strcpy(platformPrefix, "SAMD21 1W 915MHz");
+  }
+  else
+  {
+    strcpy(platformPrefix, "SAMD21 1W");
+  }
+
 #endif
 
   //Dashbord Blink LEDs
-  for (int x = 0 ; x < 2 ; x++)
+  for (int x = 0 ; x < 3 ; x++)
   {
-    digitalWrite(pin_activityLED, HIGH);
-    digitalWrite(pin_linkLED, HIGH);
+    digitalWrite(pin_rssi1LED, HIGH);
+    digitalWrite(pin_rssi2LED, HIGH);
+    digitalWrite(pin_rssi3LED, HIGH);
+    digitalWrite(pin_rssi4LED, HIGH);
     digitalWrite(pin_txLED, HIGH);
     digitalWrite(pin_rxLED, HIGH);
-    delay(100);
+    delay(50);
 
-    digitalWrite(pin_activityLED, LOW);
-    digitalWrite(pin_linkLED, LOW);
+    digitalWrite(pin_rssi1LED, LOW);
+    digitalWrite(pin_rssi2LED, LOW);
+    digitalWrite(pin_rssi3LED, LOW);
+    digitalWrite(pin_rssi4LED, LOW);
     digitalWrite(pin_txLED, LOW);
     digitalWrite(pin_rxLED, LOW);
-    delay(100);
+    delay(50);
   }
 }
 
@@ -143,9 +153,9 @@ void beginLoRa()
 
 void beginButton()
 {
-  if (pin_setupButton != 255)
+  if (pin_trainButton != 255)
   {
-    trainBtn = new Button(pin_setupButton); //Create the button
+    trainBtn = new Button(pin_trainButton); //Create the button
     trainBtn->begin();
   }
 }
@@ -160,11 +170,22 @@ void beginWDT()
 #endif
 }
 
+//Delay with pets of WDT when needed
+void delayWDT(uint16_t delayAmount)
+{
+  unsigned long startTime = millis();
+  while (millis() - startTime < delayAmount)
+  {
+    delay(1);
+    petWDT();
+  }
+}
+
 void beginSerial(uint16_t serialSpeed)
 {
-  Serial.begin(settings.serialSpeed);
+  Serial.begin(serialSpeed);
 #if defined(ARDUINO_ARCH_SAMD)
-  Serial1.begin(settings.serialSpeed);
+  Serial1.begin(serialSpeed);
 #endif
 
 #if defined(ENABLE_DEVELOPER)
