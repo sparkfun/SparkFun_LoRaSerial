c4ad8ab92ffe0df4b9f20d15fa552fcd19532ab5
Nathan Seidle <nathan@sparkfun.com>
2022-04-12 20:29:20 -0600

    Update batch_program.bat

--- Binaries/batch_program.bat
+++ Binaries/batch_program.bat
@@ -6,11 +6,21 @@ rem if [%1]==[] goto usage
 
 @echo Programming binary: %1 on %2
 
-@echo Flashing bootloader and  firmware...
-@bossac.exe -i -d --port=%2 -U true -i -e -w -v %1 -R 
+mode COM36 BAUD=1200 PARITY=n DATA=8 dtr=on
+set /p x="REALLY BIG TEST TO KEEP the port open for a smidgeREALLY BIG TEST TO KEEP the port open for a smidgeREALLY BIG TEST TO KEEP the port open for a smidgeREALLY BIG TEST TO KEEP the port open for a smidgeREALLY BIG TEST TO KEEP the port open for a smidgeREALLY BIG TEST TO KEEP the port open for a smidge" <nul >\\.\COM36
+rem echo 1>\\.\COM36
+
+bossac.exe -i -d --port=%2 -U true -i -e -w -v %1 -R
+
+@echo Flashing bootloader and firmware...
+rem @bossac.exe -a -i -d --port=%2 -U true -i -e -w -v %1 -R
+rem bossac.exe -a -i -d --port=COM36 -U true -e -w -v LoRaSerial_Firmware_v01.bin -R
+rem powershell works: $port= new-Object System.IO.Ports.SerialPort COM36,1200,None,8,one
+
 
 @echo Done programming! Ready for next board.
 rem @pause
+
 
 rem goto loop
 
