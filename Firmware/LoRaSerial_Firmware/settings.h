e2cac760d0ff86c89305fdb81139abbbde9a8e2d
Lee Leahy <lpleahyjr@gmail.com>
2022-10-12 07:55:34 -1000

    Add setting linkUpDown and command ATS57 to print link status

--- Firmware/LoRaSerial_Firmware/settings.h
+++ Firmware/LoRaSerial_Firmware/settings.h
@@ -312,6 +312,7 @@ typedef struct struct_settings {
   bool copySerial = true; //Copy the serial parameters to the training client
   bool copyTriggers = true; //Copy the trigger parameters to the training client
   uint8_t trainingKey[AES_KEY_BYTES] = { 0x53, 0x70, 0x61, 0x72, 0x6b, 0x46, 0x75, 0x6E, 0x54, 0x72, 0x61, 0x69, 0x6e, 0x69, 0x6e, 0x67 };
+  bool printLinkUpDown = false; //Print the link up and link down messages
 
   //Add new parameters immediately before this line
   //-- Add commands to set the parameters
