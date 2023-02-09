44f75c76233d5c610bdbad6dc10941e422915408
Nathan Seidle <nathan@sparkfun.com>
2023-02-09 11:03:30 -0700

    Change to jlink programmer

--- Binaries/Bootloader_Combined/batch_program.bat
+++ Binaries/Bootloader_Combined/batch_program.bat
@@ -9,13 +9,13 @@ if [%1]==[] goto usage
 @echo Programming binary: %1
 
 @echo Unlock bootloader
-atprogram.exe -t atmelice -i swd -cl 20mhz -d atsamd21g18a write --fuses --offset 0x804000 --values 0xFFC7E0D8
+atprogram.exe -t samice -i swd -cl 20mhz -d atsamd21g18a write --fuses --offset 0x804000 --values 0xFFC7E0D8
 
 @echo Programming firmware
-atprogram.exe -t atmelice -i swd -cl 20mhz -d atsamd21g18a chiperase program -f %1 --verify
+atprogram.exe -t samice -i swd -cl 20mhz -d atsamd21g18a chiperase program -f %1 --verify
 
 @echo Lock bootloader
-atprogram.exe -t atmelice -i swd -cl 20mhz -d atsamd21g18a write --fuses --offset 0x804000 --values 0xFAC7E0D8
+atprogram.exe -t samice -i swd -cl 20mhz -d atsamd21g18a write --fuses --offset 0x804000 --values 0xFAC7E0D8
 
 @echo Done programming! Ready for next board.
 @pause
